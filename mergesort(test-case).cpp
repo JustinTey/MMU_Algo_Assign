0971df91c85ca9c7185f6b26917d397da6752cb7
Wilson <59793320+wilsonlkm98@users.noreply.github.com>
2020-01-14 12:17:46 +0800

    Update mergesort(test-case).cpp

--- mergesort(test-case).cpp
+++ mergesort(test-case).cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void printArray (int *array, int size){
-	for (int i = 0; i <= size; ++i)
+	for (int i = 0; i != size; ++i)
 		cout << array[i] << ' ';
 	cout << endl;
 }
@@ -61,54 +61,47 @@ void mergeSort(int *array, int size, int p, int r){
 }
 
 void bestCase(int *array,int size){
-	//Test size between 0 to 1000.
+	//Test size between 0 to 10000.
 	for(int i = 0; i <= size; i++)
 		array[i] = i;
-	
-	//Show original output.
-	cout << "Before sort: ";
-	printArray(array,size);
-					
-	//Show sorted output.
-	cout << "\n\nAfter sorted: ";
-	auto start = chrono::system_clock::now();
-	mergeSort(array, size, 0, size);
-	auto end = chrono::system_clock::now();
-	chrono::duration<double> duration = end - start;
-			
-	printArray(array,size);
-	cout << "Duration: " << duration.count() << "s\n";	
 }
 
 void worstCase(int *array,int size){
-	//Test size between 0 to 1000.
+	//Test size between 0 to 10000.
 	int range = size;
 	for(int i = 0; i <= size; i++){
 		array[i] = range;
 		range--;
 	}
-	
-	//Show original output.
-	cout << "Before sort: ";				
-	printArray(array,size);
-	
-	//Show sorted output.	
-	cout << "\n\nAfter sorted: ";
-	auto start = chrono::system_clock::now();
-	mergeSort(array, size, 0, size);
-	auto end = chrono::system_clock::now();
-	chrono::duration<double> duration = end - start;
-			
-	printArray(array,size);
-	cout << "Duration: " << duration.count() << "s\n";
 }
 
 void averageCase(int *array, int size){
-	//Test size between 0 to 1000.
+	//Test size between 0 to 10000.
 	//Init random number generator.
 	srand(time(0));
 	for(int i = 0; i <= size; i++)
-		array[i] = (rand()%1000);
+		array[i] = (rand()%10000);
+}
+
+int main(){
+	const int size = 10000; 
+	int array[size];
+	
+	int input;
+	cout << "1 -- Best case\n2 -- Average case\n3 -- Worst case\n\nSelect the test case: ";
+	cin >> input;
+	
+	switch(input){
+		case 1:	bestCase(array,size);
+			break;
+			
+		case 2:	averageCase(array,size);
+			break;
+
+		case 3:	worstCase(array,size);
+			break;
+				
+	}
 	
 	//Show original output.
 	cout << "Before sort: ";					
@@ -120,33 +113,9 @@ void averageCase(int *array, int size){
 	mergeSort(array, size, 0, size);
 	auto end = chrono::system_clock::now();
 	chrono::duration<double> duration = end - start;
-			
+		
 	printArray(array,size);
 	cout << "Duration: " << duration.count() << "s\n";
-}
 
-int main(){
-	const int size = 1000; 
-	int array[size];
-	int input;
-	
-	cout << "1 -- Best case\n2 -- Average case\n3 -- Worst case\n\nSelect the test case: ";
-	cin >> input;
-	
-	switch(input){
-		case 1:	bestCase(array,size);
-				break;
-			
-		case 2:	averageCase(array,size);
-				break;
-
-		case 3:	worstCase(array,size);
-				break;
-				
-	}
-	
 	return 0;
 }
-
-
-
