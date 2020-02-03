460a7317c8fb42d7be3c5ddce37a3b86b2d2d8bb
foofangjee <51477859+fangjeefoo@users.noreply.github.com>
2020-02-03 21:19:18 +0800

    Update fixedPivot(testCase).cpp

--- fixedPivot(testCase).cpp
+++ fixedPivot(testCase).cpp
@@ -4,18 +4,16 @@
 #include <time.h>
 using namespace std;
 
-
 void generateArray (int A[], int size){
 	for(int i = 0; i < size; i++)
 		 A[i] = i+1;
 }
 
 void generateArray2(int A[], int size){
-	srand(1); //time(NULL) for random
+	srand(0); //time(NULL) for random
 
 	for(int i = 0; i < size; i++)
-        A[i] = rand() % 10 + 1;
-		// A[i] = rand() % 10000 + 1; //range of 1 to 10000
+        A[i] = rand() % size + 1; // A[i] = rand() % 10000 + 1; //range of 1 to 10000
 }
 
 int partition(int arr[], int p, int r, char flag) {
@@ -28,9 +26,9 @@ int partition(int arr[], int p, int r, char flag) {
     else
         pivotIndex = r;
 
-	swap ( arr[pivotIndex], arr[r] );
-	pi = arr[r];
+	pi = arr[pivotIndex];
 	swapIndex = p;
+	swap ( arr[pivotIndex], arr[r] );
 
 	for (int i = p; i < r; i++){
 		if ( arr[i] < pi ){
@@ -59,9 +57,9 @@ int quickSelectSort(int A[], int p, int k, int r, char flag){
 
 int main(){
 	int k;
-	const int ARRAY_SIZE = 10;
+	const int ARRAY_SIZE = 10000;
 	int choice;
-	int A[ARRAY_SIZE];
+	int* A = new int[ARRAY_SIZE];
 
 	cout << "1 -- Best case\n2 -- Average case\n3 -- Worst case\n\nSelect the test case: ";
 	cin >> choice;
@@ -70,18 +68,16 @@ int main(){
          cout << "Invalid choice"<<endl;
          exit(0);
     }
-
+    generateArray2(A,ARRAY_SIZE);
 	cout << "Please input the kth smallest element: ";
 	cin >> k;
+
 
 	if(choice == 1 || choice == 3)
         generateArray(A,ARRAY_SIZE);
     else if(choice == 2)
         generateArray2(A,ARRAY_SIZE);
-    for(int i = 0; i < ARRAY_SIZE; i++)
-        cout << A[i]<<"    ";
-    cout << endl;
-	
+
 	auto start = chrono::system_clock::now();
 	if(choice == 1)
         cout << "The " << k << "th smallest element is: " << quickSelectSort(A, 0, k - 1, ARRAY_SIZE - 1, 'b') << endl;
@@ -90,10 +86,7 @@ int main(){
     else if(choice == 3)
         cout << "The " << k << "th smallest element is: " << quickSelectSort(A, 0, k - 1, ARRAY_SIZE - 1, 'w') << endl;
 	auto end = chrono::system_clock::now();
-	
-	chrono::duration<double> duration = end - start;
-    for(int i = 0; i < ARRAY_SIZE; i++)
-        cout << A[i]<<"    ";
 
+ 	chrono::duration<double> duration = end - start;
  	cout << "\nDuration: "<<duration.count()<<endl;
 }
