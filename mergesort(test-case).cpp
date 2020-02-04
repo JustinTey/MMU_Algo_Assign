efd29b8204d05a01cb417059cfe6581606fe5bb5
foofangjee <foofangjee@gmail.com>
2020-02-04 22:50:29 +0800

    update worse case

--- mergesort(test-case).cpp
+++ mergesort(test-case).cpp
@@ -11,22 +11,22 @@ void printArray (int *array, int size){
 }
 
 void merge(int *array, int size, int p,int r){
-    
+
 	//Set the temp object size.
     int* temp = new int[size];
-	
+
 	//Midpoint of the array.
 	int mid = (p + r)/2;
-   
+
     int i = p;       // Initial point.
     int j = mid+1;   // Point after midpoint.
-    int k = p;       // Initial point for temp 
-    
+    int k = p;       // Initial point for temp
+
     while((i <= mid) && (j <= r)){
-		if(array[i] < array[j]){  			
+		if(array[i] < array[j]){
             temp[k++] = array[i++];
         }
-        else{						
+        else{
             temp[k++] = array[j++];
         }
     }
@@ -36,12 +36,12 @@ void merge(int *array, int size, int p,int r){
     while(j <= r){
         temp[k++]  = array[j++];
     }
-    
+
     //Copy all element from temp(sorted) to original arrays.
     for(int i = p; i <= r; i++){
         array[i] = temp[i];
-    } 
-	
+    }
+
 	delete temp;
 	temp = NULL;
 }
@@ -53,13 +53,13 @@ void mergeSort(int *array, int size, int p, int r){
 		//End the program
 		return;
 	}
-	
+
 	//Find the midpoint of the array.
 	int mid = (p + r)/2;
-	
+
 	mergeSort(array, size, p, mid);
 	mergeSort(array, size, mid + 1, r);
-	
+
 	//Merge the two parts.
 	merge(array, size, p, r);
 }
@@ -72,11 +72,24 @@ void bestCase(int *array,int size){
 
 void worstCase(int *array,int size){
 	//Test size between 0 to 10000.
-	int range = size;
-	for(int i = 0; i < size; i++){
-		array[i] = range;
-		range--;
-	}
+//	int range = size;
+//	for(int i = 0; i < size; i++){
+//		array[i] = range;
+//		range--;
+//	}
+    int counter = size/2;
+    int leftCounter = 0, rightCounter = 0;
+
+    for(int i = 0; i < size; i++){
+        if(i % 2 == 0){  //prove is even number
+            array[rightCounter+counter] = i;
+            rightCounter++;
+        }
+        else{
+            array[leftCounter] = i;
+            leftCounter++;
+        }
+    }
 }
 
 void averageCase(int *array, int size){
@@ -88,17 +101,17 @@ void averageCase(int *array, int size){
 }
 
 int main(){
-	const int size = 500000; 
+	const int size = 500000;
 	int* array = new int[size];
-	
+
 	int input;
 	cout << "1 -- Best case\n2 -- Average case\n3 -- Worst case\n\nSelect the test case: ";
 	cin >> input;
-	
+
 	switch(input){
 		case 1:	bestCase(array,size);
 			break;
-			
+
 		case 2:	averageCase(array,size);
 			break;
 
@@ -106,31 +119,31 @@ int main(){
 			break;
 		default : cout<<"Input incorrect exiting program";
 			exit(0);
-				
+
 	}
-	
+
 	//Write output into a txt for checking purposes
 	ofstream outFile;
 	outFile.open("check.txt");
 	for (int i =0; i < size; i++)
 		outFile << array[i] << " ";
 	outFile.close();
-	
+
 	//retrive k-element to find
 	cout<<"Which element would you like to find in the array?"<<endl;
 	cin>>input;
 	//Show original output.
-	//cout << "Before sort: ";					
+	//cout << "Before sort: ";
 	//printArray(array,size);
-	
-	//Show sorted output.	
+
+	//Show sorted output.
 	//cout << "\n\nAfter sorted: ";
 	auto start = chrono::system_clock::now();
 	mergeSort(array, size, 0, size - 1);
 	int answer = array[input-1];
 	auto end = chrono::system_clock::now();
 	chrono::duration<double> duration = end - start;
-		
+
 	//printArray(array,size);
 	cout << "Duration: " << duration.count() << "s\n";
 
